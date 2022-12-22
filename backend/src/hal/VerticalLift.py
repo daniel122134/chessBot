97be23a176d11c00387c7551dcf21cd830081342
daniel haddad <daniel.haddad@yahooinc.com>
2022-12-22 17:44:04 +0200

    added move api

--- backend/src/hal/VerticalLift.py
+++ backend/src/hal/VerticalLift.py
@@ -4,16 +4,22 @@ from gpiozero import DigitalOutputDevice
 
 
 class VerticalLift:
-    def __init__(self, lift_pin, lower_pin):
-        self.lift_pin_device = DigitalOutputDevice(lift_pin)
-        self.lower_pin_device = DigitalOutputDevice(lower_pin)
+    def __init__(self, lift_pin_pos, lower_pin_pos, lift_pin_neg, lower_pin_neg):
+        self.lift_pin_device_pos = DigitalOutputDevice(lift_pin_pos)
+        self.lift_pin_device_neg = DigitalOutputDevice(lift_pin_neg)
+        self.lower_pin_device_pos = DigitalOutputDevice(lower_pin_pos)
+        self.lower_pin_device_neg = DigitalOutputDevice(lower_pin_neg)
 
     def lift(self):
-        self.lift_pin_device.on()
+        self.lift_pin_device_pos.on()
+        self.lift_pin_device_neg.on()
         time.sleep(1)
-        self.lift_pin_device.off()
+        self.lift_pin_device_pos.off()
+        self.lift_pin_device_neg.off()
 
     def lower(self):
-        self.lower_pin_device.on()
+        self.lower_pin_device_pos.on()
+        self.lower_pin_device_neg.on()
         time.sleep(1)
-        self.lower_pin_device.off()
+        self.lower_pin_device_pos.off()
+        self.lower_pin_device_neg.off()
--- backend/src/hal/config/devices.py
+++ backend/src/hal/config/devices.py
@@ -1,8 +1,8 @@
 from backend.src.hal.VerticalLift import VerticalLift
 from backend.src.hal.engine import Engine
 
-STEP_PIN_VER = 1
-DIR_PIN_VER = 2
+STEP_PIN_VER = 15
+DIR_PIN_VER = 14
 
 STEP_PIN_HOR = 1
 DIR_PIN_HOR = 2
@@ -14,6 +14,8 @@ MODE_PIN3 = 5
 vertical_engine1 = Engine(STEP_PIN_VER, DIR_PIN_VER, MODE_PIN1, MODE_PIN2, MODE_PIN3)
 horizontal_engine1 = Engine(STEP_PIN_HOR, DIR_PIN_HOR, MODE_PIN1, MODE_PIN2, MODE_PIN3)
 
-LIFT_PIN = 1
-LOWER_PIN = 2
-lift = VerticalLift(LIFT_PIN, LOWER_PIN)
+LIFT_PIN_POS = 2
+LOWER_PIN_POS = 3
+LIFT_PIN_NEG = 4
+LOWER_PIN_NEG = 17
+lift = VerticalLift(LIFT_PIN_POS, LOWER_PIN_POS, LIFT_PIN_NEG, LOWER_PIN_NEG)
