0cd24cc1d790676d95c76b321166fe287b3d9303
daniel haddad <daniel.haddad@yahooinc.com>
2023-01-05 17:34:52 +0200

    testing 123

--- backend/src/hal/config/devices.py
+++ backend/src/hal/config/devices.py
@@ -7,7 +7,7 @@ DIR_PIN_VER = 24
 vertical_engine1 = Engine(STEP_PIN_VER, DIR_PIN_VER, direction=False)
 
 
-STEP_PIN_VER = 8
+STEP_PIN_VER = 26
 DIR_PIN_VER = 18
 vertical_engine2 = Engine(STEP_PIN_VER, DIR_PIN_VER)
 
