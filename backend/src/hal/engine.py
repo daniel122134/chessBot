d0c4fca03774a1f2477da162e91a43f72d00926d
daniel haddad <daniel.haddad@yahooinc.com>
2023-01-05 18:24:12 +0200

    testing 123

--- backend/src/hal/engine.py
+++ backend/src/hal/engine.py
@@ -20,6 +20,7 @@ class Engine:
         sleep_time = interval 
         print(steps)
         print(self.direction)
+        print(self.dir_pin.is_active)
         if steps < 0:
             self.change_dir()
 
