8a117d8bee20292fe528e0f5a520226b8e8619ed
daniel haddad <daniel.haddad@yahooinc.com>
2023-01-12 01:09:28 +0200

    lift api

--- backend/src/hal/WizardsChessController.py
+++ backend/src/hal/WizardsChessController.py
@@ -12,7 +12,7 @@ directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
 class WizardsChessController:
 
     def __init__(self):
-        self.grid = GridControl(10, 10, 8, 8, 5, 5, vertical_engines, horizontal_engines)
+        self.grid = GridControl(10, 8, 8, 5, 5, vertical_engines, horizontal_engines)
         self.lift = lift
 
     def move_piece(self, src, dst, board: Board):
